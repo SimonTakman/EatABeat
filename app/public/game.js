13ee54da0d4ebf372fa595c7c897c9fda2bbf2db
SimonTakman <simontakman@gmail.com>
2018-10-13 15:01:52 +0200

    DAgs för problem

--- app/public/game.js
+++ app/public/game.js
@@ -1,14 +1,32 @@
 var height = document.body.height;
 var width = document.body.width;
 
-var app = new PIXI.Application({backgroundColor : 0x1099bb, autoResize: true });
+var app = new PIXI.Application({backgroundColor : 0x1DB954, autoResize: true });
 var gameViewElement = document.getElementById('game-view');
 gameViewElement.appendChild(app.view);
+app.stop();
 
 let obstacles = [];
-
+var position = 0
 let data = JSON.parse(mock);
 let beats = data.analysis.beats;
+var background, background2; 
+
+PIXI.loader.load(onAssetsLoader);
+
+function onAssetsLoader(loader,res){
+  background = PIXI.Sprite.fromImage('assets/sprites/background_v2.png')
+  background2 = PIXI.Sprite.fromImage('assets/sprites/background2_v2.png')
+  app.stage.addChild(background)
+  app.stage.addChild(background2)
+  app.start();
+}
+
+
+/*background.scale.x = 1.25
+background.scale.y = 1.25
+background2.scale.x = 1.25
+background2.scale.y = 1.25*/
 console.log(beats);
 
 function addObstacle(width, height, xPosition) {
@@ -30,6 +48,25 @@ function getDuration() {
 let lastBeat = -1;
 // Listen for animate update
 app.ticker.add(function(delta) {
+
+    position += 10;
+
+    background.y = -(position * 0.6);
+    background.y %= 1024 * 2;
+    if(background.y < 0)
+    {
+        background.y += 1024 * 2;
+    }
+    background.y -= 1024;
+
+    background2.y = -(position * 0.6) + 1024;
+    background2.y %= 1024 * 2;
+    if(background2.y < 0)
+    {
+        background2.y += 1024 * 2;
+    }
+    background2.y -= 1024;
+
     // just for fun, let's rotate mr rabbit a little
     // delta is 1 if running at 100% performance
     // creates frame-independent transformation
@@ -56,7 +93,7 @@ app.ticker.add(function(delta) {
 
     if (newBeat != lastBeat) {
         lastBeat = newBeat;
-        addObstacle(100, 50, Math.random());
+        //addObstacle(100, 50, Math.random());
     }
     else if (lastBeat == beats.length - 1) {
         console.log("Done with beats");
