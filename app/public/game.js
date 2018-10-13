01e4539a8f2e1fcf39703baed83e6675ed7be367
Iman Radjavi <radjavi@hotmail.com>
2018-10-13 21:20:44 +0200

    speed fix

--- app/public/game.js
+++ app/public/game.js
@@ -120,7 +120,6 @@ class Game {
         if (newBarIndex != this.status.barIndex) {
             this.status.barIndex = newBarIndex;
             var factor = beats[newBarIndex].confidence;
-            console.log(factor);
             var width = factor * 200 + 50;
             var height = factor * 100 + 25;
             this.addObstacle(width, height, Math.random());
@@ -132,9 +131,10 @@ class Game {
         // Calculate speed from tempo
         let minTempo = 90;
         let maxTempo = 160;
+        console.log(this.track.features.tempo);
         let tempo = Math.max(Math.min(this.track.features.tempo, maxTempo), minTempo)
         let tempoScaled = (tempo - minTempo) / (maxTempo - minTempo);
-        this.speed = tempoScaled * 10 + 0.5;
+        this.speed = tempoScaled * 5 + 5;
         //this.track.analysis.bars = [this.track.analysis.bars[0], this.track.analysis.bars[1]];
     }
 
